0d7aa5bc9f25f7b2869dd8106cefda26f08fda41
“jackjack” <eugenkanev@gmail.com>
2017-09-10 20:36:37 +0300

    made header without adaptive

--- resources/views/core/base.blade.php
+++ resources/views/core/base.blade.php
@@ -11,6 +11,7 @@
     <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
     <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('css/custom.css')}}">
+    <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
 </head>
 <body class="{{$body_class}}">
 <div class="wrapper">
--- resources/views/parts/_header.blade.php
+++ resources/views/parts/_header.blade.php
@@ -1,1 +1,25 @@
-<h4>header.blade</h4>
+<div class="header">
+	<div class="header-container">
+		<div class="header-t">
+			<a href="/" class="header-logo">Nova</a>
+			<div class="header-t-l">
+			<div class="header-tel">
+					<a href="#" class="header-tel-i">0939200290</a>
+					<a href="#" class="header-tel-i">0635825870</a>
+				</div>
+				<a href="#" class="header-soc fa fa-vk fa-2x" target="blank"></a>
+				<a href="#" class="header-soc fa fa-facebook-square fa-2x" target="blank"></a>
+				<a href="#" class="header-soc fa fa-instagram fa-2x" target="blank"></a>
+				
+			</div>
+			<div class="header-t-r">
+				<div class="header-search">
+					<input type="text" placeholder="Найти товары" class="header-search-inp">
+					<a href="#" class="header-search-ic fa fa-search"></a>
+				</div>
+				<a href="#" class="header-like">Избранное<span class="header-;like-num">(0)</span></a>
+				<a href="#" class="header-busket">Корзина <span class="header-busket-num">(0)</span></a>
+			</div>
+		</div>
+	</div>
+</div>
