7376292af5834752d9ff9245a5279e974dbcaffe
jackjack2017 <eugenkanev@gmail.com>
2018-01-15 19:00:45 +0200

    added badges, properties to products

--- resources/views/parts/_product-card.blade.php
+++ resources/views/parts/_product-card.blade.php
@@ -1,7 +1,10 @@
 <a href="#" class="product-card">
 	<div class="product-card-t">
 		<img src="{{asset('img/product-img2.jpg')}}" alt="banner">
-		<button class="product-card-t-ic fa fa-heart-o fa-2x"  aria-hidden="true"></button >
+		<button class="product-card-t-ic __favourite fa fa-heart-o fa-2x"  aria-hidden="true"></button >
+		<span class="product-card-t-ic __new"  aria-hidden="true">NEW</span >
+		<span class="product-card-t-ic __top"  aria-hidden="true">TOP</span >
+		<span class="product-card-t-ic __discount fa fa-percent fa-2x"  aria-hidden="true"></span >
 	</div>
 	<div class="product-card-b">
 		<div class="product-card-btn-inverse-wrap">
